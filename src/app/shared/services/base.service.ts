be6694b0ab110786a70cbae75e14b08eeb8b00ff
Piero Elescano <pieroelescano300@gmail.com>
2025-09-30 17:03:39 -0500

    chore: add url of the data server

--- src/app/shared/services/base.service.ts
+++ src/app/shared/services/base.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class BaseService {
   protected readonly http = inject(HttpClient);
-  protected readonly apiUrl = 'http://localhost:3000';
+  protected readonly apiUrl = environment.apiUrl;
 
   protected get<T>(endpoint: string) {
     return this.http.get<T>(`${this.apiUrl}/${endpoint}`);
